425e84aa438b3363bb015e6efbbd847ea9bbcaa3
kento <kento.kawahara@rakuten-sec.co.jp>
2021-09-30 15:13:15 +0900

    3-13

--- app/ProfileHistory.php
+++ app/ProfileHistory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProfileHistory extends Model
 {
     protected $guarded = array('id');
+    protected $table = '_profile_histories';
 
     public static $rules = array(
         'profile_id' => 'required',
